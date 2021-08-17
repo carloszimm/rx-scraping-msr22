e92b1a3e38a15b799a81651aa49688e3426bd7df
Carlos Zimmerle <cezl@cin.ufpe.br>
2021-08-17 16:08:36 -0300

    feat: add rxdart scraping code

--- scrape.js
+++ scrape.js
@@ -1,9 +1,11 @@
 const scrapeRxDoc = require("./lib/reactivex.io/scrape-operators"),
-  scrapeRxGo = require("./lib/rxgo/scrape-operators");
+  scrapeRxGo = require("./lib/rxgo/scrape-operators"),
+  scrapeRxDart = require("./lib/rxdart/scrape-operators");
 
-async function main(){
-    await scrapeRxDoc();
-    scrapeRxGo();
+async function main() {
+  await scrapeRxDoc();
+  await scrapeRxGo();
+  scrapeRxDart();
 }
 
 main();
