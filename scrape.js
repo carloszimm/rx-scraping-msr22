5cfcc0e655e3a65a30bfdb4a4f8a8b9fac7a1de4
Carlos Zimmerle <carloszimmerle@gmail.com>
2021-09-27 22:21:22 -0300

    feat: add rxkotlin to the scraping

--- scrape.js
+++ scrape.js
@@ -2,14 +2,16 @@ const scrapeRxDoc = require("./lib/reactivex.io/scrape-operators"),
   scrapeRxGo = require("./lib/rxgo/scrape-operators"),
   scrapeRxDart = require("./lib/rxdart/scrape-operators"),
   scrapeRxJS = require("./lib/rxjs/scrape-operators"),
-  scrapeRxJava = require("./lib/rxjava/scrape-operators");
+  scrapeRxJava = require("./lib/rxjava/scrape-operators"),
+  scrapeRxKotlin = require("./lib/rxkotlin/scrape-operators");
 
 async function main() {
   scrapeRxDoc();
   scrapeRxDart();
   scrapeRxGo();
   scrapeRxJS();
-  scrapeRxJava();
+  const rxJavaOpertors = await scrapeRxJava();
+  scrapeRxKotlin(rxJavaOpertors);
 }
 
 main();
